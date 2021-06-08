9c4a39b11c52be8379297f542402586e731d8ccd
Yonghwi Kim <yh0921k@gmail.com>
2021-06-08 11:14:22 +0900

    [init] TestBed Project 초기화
    module
    - SpringBoot   : 2.5.0
    - Database     : MariaDB, H2
    - Build Tool   : Gradle
    - Language     : Java 11
    
    start.spring.io를 이용한 프로젝트 생성
    - Dependencies  : Spring Web, Spring Data JPA, Validation
                    : Lombok, H2 Database, MariaDB Driver
    		: SpringBoot DevTools, Spring Data Redis
    
    .gitignore 추적 제외 파일 설정
    - IntelliJ, Java, Database, VisualStudioCode
    - Git, Gradle, Eclipse
    - macOS, Windows, Linux

---
+++ src/main/java/com/testbed/TestbedApplication.java
@@ -0,0 +1,13 @@
+package com.testbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testbed/TestbedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testbed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
