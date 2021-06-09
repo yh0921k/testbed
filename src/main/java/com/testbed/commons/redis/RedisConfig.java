763aaef0e2b5a5a7ca18a4821bf9f288234f7de3
Yonghwi Kim <yh0921k@gmail.com>
2021-06-09 16:18:24 +0900

    [add] 간단한 RedisRepository 구현 및 테스트

--- src/main/java/com/testbed/commons/redis/RedisConfig.java
+++ src/main/java/com/testbed/commons/redis/RedisConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
   @Bean
