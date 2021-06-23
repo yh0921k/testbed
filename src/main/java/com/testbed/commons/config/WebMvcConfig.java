a54cf3ff5b7fd582e0299b8605720c9020f47299
Yonghwi Kim <yh0921k@gmail.com>
2021-06-23 13:26:55 +0900

    [add] JWT를 검증할 인터셉터 및 설정 구현
    AuthInterceptor.java
    - 토큰이 비어있을 경우 AuthenticationException 발생
    - 토큰이 유효하지 않을 경우 JwtRuntimeException 발생
    - 권한이 맞지 않을 경우 AuthorizationException 발생
    
    WebMvcConfig.java
    - 모든 요청에 대해 인즈이 필요하지만 회원가입, 로그인, Refresh Token 재발급의 경우는 인증 불필요
    - 위의 인터셉터 등록

---
+++ src/main/java/com/testbed/commons/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.testbed.commons.config;
+
+import com.testbed.commons.jwt.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  private final AuthInterceptor authInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(authInterceptor)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/sign-up", "/sign-in", "/users/refresh");
+  }
+}
